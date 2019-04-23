1cca5844db790ddf9a27e21f241cec1f2cd5b2c9
stellaxxy <stellaxxy0109@gmail.com>
2019-04-23 11:26:56 -0700

    first practice count occurences

---
+++ first practice/main.js
@@ -0,0 +1,19 @@
+/*
+
+countOccurences:
+
+given an array of words and a separate word, count how many times a particular word given occurs within that array for example:
+input ['dog','cat','dog','pig','canary','cat','dog'], 'dog'
+returns 3
+
+ */
+
+function countOccurences(array, word) {
+    let count = 0;
+    for(let index = 0; index < array.length; index++){
+        if(array[index] === word) {
+            count++;
+        }
+    }
+    return count;
+}
